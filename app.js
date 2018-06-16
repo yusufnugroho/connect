2cbf125e7fbb6073d55fbf39458ea7191a721517
yusuf_nugroho <kotakkode@gmail.com>
2018-06-17 02:05:49 +0700

    express render engine from jade to pug

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var logger = require('morgan')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
+var pug = require('pug')
 
 var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set('view engine', 'pug')
 
 app.use(logger('dev'))
 app.use(express.json())
